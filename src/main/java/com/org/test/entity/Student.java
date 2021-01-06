07b7a2cffc7ed213ca857b4c355bb8ccdc4efa97
Amrat C <amratc@192.168.1.7>
2021-01-06 16:21:17 +0530

    initial commit

---
+++ src/main/java/com/org/test/entity/Student.java
@@ -0,0 +1,7 @@
+package com.org.test.entity;
+
+public class Student {
+    private int id;
+    private String name;
+    private String address;
+}
---
+++ src/main/java/com/org/test/service/StudentService.java
@@ -0,0 +1,9 @@
+package com.org.test.service;
+
+import com.org.test.entity.Student;
+
+public class StudentService {
+    public static void main(String args[]){
+        Student s = new Student();
+    }
+}
